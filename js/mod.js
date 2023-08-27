71523678b5584ae0bb12cc63457d27d2a940cc15
Day Light <nik.dani1@gmail.com>
2023-08-27 18:15:42 +0200

    Update mod.js
    Custom name and id

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Chicken Tree",
+	id: "chickentree",
+	author: "Daylight",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
